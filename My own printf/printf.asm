f5fe1cf2d4492f4308ee2921323e4b82bbfc7468
Xenia <heiduk.k.k.s@yandex.ru>
2021-03-04 18:16:09 +0300

    %d is output (great crutch)

--- My own printf/printf.asm
+++ My own printf/printf.asm
@@ -2,11 +2,22 @@
 ; nasm -f elf64 -l printf.lst printf.asm
 ; ld -s -o printf printf.o
 
+; r8  - 
+; r9  - number_to_itoa
+; r10 - system_to_itoa
+; r11 - format string
+; r12 - 
+; r13 - begin of the parameters
+; r14 - length_number_to_itoa
+; r15 - 
+
 section .text
 
 global _start                  
 
 _start:    
+			push '('
+			push 3802
 			push ')'
 			push '#'
 			push string_to_print
@@ -78,6 +89,9 @@ percent_handler:
     		cmp dh, '%'
     		je printf_procent
 
+    		cmp dh, 'd'
+    		je decimal_handler    		
+
 
     		jmp global_handler
 
@@ -97,7 +111,7 @@ char_handler:
             pop rcx
             inc r11
 
-            add r13, 8
+            add r13, 1 * 8
 
     		xor rcx, rcx
 
@@ -117,7 +131,51 @@ printf_procent:
 
     		xor rcx, rcx
 
-            jmp global_handler              
+            jmp global_handler  
+
+decimal_handler:
+
+			mov r9, rbp
+			add r9, r13
+			add r13, 1 * 8
+
+			mov r10, 10
+			mov rax, [r9]
+			call itoa
+
+			mov r8, rcx
+			mov r9, r11
+
+			mov rsi, rsp		
+
+	print_numbres:
+			mov al, byte [rsi]
+			cmp al, '$'
+			je end_print_decimal
+
+			mov rax, 0x01       ; write64 (rdi, rsi, rdx) ... r10, r8, r9
+            mov rdi, 1          ; stdout
+            mov rdx, 1    		; strlen 
+            syscall	 
+
+            pop r14
+
+            add rsi, 1 * 8
+            ;inc rsi
+            jmp print_numbres
+
+    end_print_decimal:
+    		pop r14				; delete '$'
+
+            mov r11, r9
+            mov rcx, r8
+
+            inc r11
+
+    		xor rcx, rcx
+    		;add rsp, r14 * 8
+
+            jmp global_handler  
 
 
 end_printf:
@@ -132,10 +190,50 @@ end_printf:
 
             ret
 
+
+itoa:
+			pop r8
+
+			xor r14, r14
+			push '$'
+
+			cmp r9, 0x00
+			jne itoa_loop
+
+			push '0'
+			inc r14
+			jmp end_itoa_func
+
+		itoa_loop:	
+
+			cmp rax, 0x00
+			je end_itoa_func
+			
+			mov rdx, 0x00
+			div r10
+			add rdx, '0'
+
+			cmp rdx, '9'
+			jbe is_a_digit
+
+			add rdx, 'A' - '9' - 1
+			
+		is_a_digit:
+
+			push rdx
+			inc r14
+			jmp itoa_loop
+
+		end_itoa_func:			
+
+			push r8
+	 
+			ret         
+
             
 section     .data
             
-string_to_print	db "L%cOL%cHel%%lo, world!%%", 0x00
+string_to_print	db "L%cOL%cHel%%lo%d, %cworld!%%", 0x00
 
 null_symbol			db 0
 
