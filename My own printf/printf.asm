1cc9be2b734a8ed807f3eb9fe5137ba5b3fe61aa
Xenia <heiduk.k.k.s@yandex.ru>
2021-03-04 16:46:20 +0300

    %c is output

--- My own printf/printf.asm
+++ My own printf/printf.asm
@@ -7,9 +7,11 @@ section .text
 global _start                  
 
 _start:    
+			push ')'
+			push '#'
 			push string_to_print
 			call printf
-			;add rsp, 1
+			add rsp, 2 * 8
 			;pop r13
 
 
@@ -18,43 +20,101 @@ _start:
             syscall
 
 printf:
-			pop r12				; address of return
+			enter 0, 0
 
-			mov r11, rbp
-			mov rbp, rsp
+			mov r11, [rbp + 2 * 8]		; format string
+			mov rbx, 0
+			mov rcx, 0
 
-			pop	rbx			
+			mov	r13, 3 * 8			; begin of the parameters
 
 			
-	global_handler:
+global_handler:
 
+			mov rax, r11
+			add rax, rcx
 
-			cmp rbx, null_symbol
+			mov dl, byte [rax]
+
+			cmp dl, 0x00
 			je end_printf
 
+			cmp dl, '%'
+			je percent_handler			
 
-		    mov rax, 0x01       ; write64 (rdi, rsi, rdx) ... r10, r8, r9
-            mov rdi, 1          ; stdout
-            mov rsi, rbx
-            mov rdx, 1    		; strlen 
-            syscall	
-
-            inc rbx
+		    inc rcx				; length message
 
             jmp global_handler	
 
-    end_printf:
+percent_handler:
 
-            push r12
+			push rcx
+			push rdx
+			push r11
+			push rax
 
-            nop
+			mov rax, 0x01       ; write64 (rdi, rsi, rdx) ... r10, r8, r9
+            mov rdi, 1          ; stdout
+            mov rsi, r11
+            mov rdx, rcx   		; strlen 
+            syscall	
+
+            pop rax
+            pop r11
+            pop rdx
+            pop rcx
+
+    		add r11, rcx
+    		inc r11
+
+    		xor rcx, rcx
+
+    		inc rax
+    		mov dh, byte [rax]
+
+    		cmp dh, 'c'
+    		je char_handler
+
+    		jmp global_handler
+
+char_handler:
+
+			push rcx
+			push r11
+			
+			mov rax, 0x01       ; write64 (rdi, rsi, rdx) ... r10, r8, r9
+            mov rdi, 1          ; stdout
+            mov rsi, rbp
+            add rsi, r13
+            mov rdx, 1    		; strlen 
+            syscall	 
+
+            pop r11
+            pop rcx
+            inc r11
+
+            add r13, 8
+
+    		xor rcx, rcx
+
+            jmp global_handler  		
+
+end_printf:
+			
+			mov rax, 0x01       ; write64 (rdi, rsi, rdx) ... r10, r8, r9
+            mov rdi, 1          ; stdout
+            mov rsi, r11
+            mov rdx, rcx    		; strlen 
+            syscall	 
+
+            leave
 
             ret
 
             
 section     .data
             
-string_to_print	db "Hello, world!", 0
+string_to_print	db "L%cOL%cHello, world!", 0x00
 
 null_symbol			db 0
 
