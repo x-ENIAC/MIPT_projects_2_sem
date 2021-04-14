23203bdfeca16890bdf957a36023ec84df3f6133
Xenia <heiduk.k.k.s@yandex.ru>
2021-04-14 19:33:25 +0300

    Fixed assembler insertions

--- Simple examples/Assembler insertion/main.cpp
+++ Simple examples/Assembler insertion/main.cpp
@@ -1,33 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
-char* _strcpy(char* memory, const char* string_for_copy) {
-	char* result = memory;
+unsigned long long get_hash_word(const char* word, const int length_word) {
+	if(!word || length_word < 0)
+		return 1;
 
-	__asm__ ( "   mov rsi, %0	; "
-			  "   mov rdi, %1	; "
-			  "n: mov al, [rsi]	; "
-			  "	  mov [rdi], al	; "
-			  "   or al, 0		; "
-			  "	  jz e 			; "
-			  "	  inc rsi		; "
-			  "   inc rdi		; "
-			  "	  jmp n 		; "
-			  " e:				; "
-			  :: "m"(string_for_copy), "m"(memory)	// переменные, которые доступны вставке
-			  :"%rdi", "%rsi", "%rax"	// список разрушаемых, затрагиваемых регистров
-		);
+	unsigned long long hash = 0, delta = 1;
 
-	return result;
+	for(int pos = 0; pos < length_word; ++pos) {		
+		hash = hash + (word[pos] - 'a' + 1) * delta;
+		delta += 1000;
+		//printf("%c - %lld (%d, %d)\n", word[pos], hash, word[pos], word[pos] - 'a' + 1);
+	}
+
+	return hash;
+}
+
+void get_hash(const char* my_string, const int length) {
+
+	unsigned long long hash = 0;
+	unsigned long long delta = 1;
+
+	asm ("mov r12, %1		; " // r12 = my_string
+		 "xor r10, r10		; " // r10 = hash
+		 "mov rbx, %2		; "	// rbx = delta
+		 "mov rcx, 4		; " // rcx = length
+		 "xor r11, r11		; "
+
+	  "get_hash:			; "
+		 "cmp rcx, 0		; "
+		 "je end_get_hash	; "
+		 "mov r11b, [r12]	; "
+		 "sub r11b, 96		; "
+		 "mov rax, r11		; "
+		 "mul rbx			; "
+		 "add rbx, 1000		; "
+		 "add r10, rax		; "
+		 "inc r12			; "
+		 "dec rcx			; "
+		 "jmp get_hash 		; "
+
+	  "end_get_hash:		; "
+	  	 "mov %0, r10		; "
+	    : "=r" (hash)										// something will be written to these variables 
+	    : "r" (my_string), "r"(delta), "r"(length)			// something will be read from these variables 
+	    : "rax", "rbx", "rcx", "r10", "r11", "r12"			// trash-list
+	);
+
+	printf("%lld\n", hash);
+
+	return;
 }
 
 int main() {
 
-    char* copied = (char*)calloc(64, sizeof(char));
-    copied = _strcpy(copied, "Hello, world!");
-    printf("%s\n", copied);
+    get_hash("heir", 4);
 
-    free(copied);
+    printf("%lld\n", get_hash_word("heir", 4));
 
     return 0;
 }
